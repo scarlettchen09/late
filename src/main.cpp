a4baee879179cb6a7e9bbb8fd0aa7170036be7d5
Scarlett <scarlettch09@gmail.com>
2020-02-16 21:10:28 -0800

    Better jumping

--- src/main.cpp
+++ src/main.cpp
@@ -1,162 +1,145 @@
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
-#include <SFML/Audio.hpp>
-#include <string>
 #include <iostream>
+#include <string>
 int main()
 {
-    sf::Vector2i screenDimensions(800, 600);
+	sf::Vector2i screenDimensions(800, 600);
 
-    sf::RenderWindow Window;
-    Window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "My First SFML Game");
-    Window.setKeyRepeatEnabled(false);
-    sf::Texture bTexture;
-    sf::Sprite bImage;
-    sf::SoundBuffer jump;
-    sf::Music music;
-    if(!bTexture.loadFromFile("../resources/Background.jpg"))
-      std::cout << "Could not load background image" << std::endl;
-    if (!music.openFromFile("../resources/background.wav"))
-        std::cout << "Could not load background music" << std::endl;
-    if (!jump.loadFromFile("../resources/jump.wav"))
-        std::cout << "Could not load jump sound effect" << std::endl;
-    sf::Sound sound;
-    sound.setBuffer(jump);
+	sf::RenderWindow Window;
+	Window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Late!");
+	Window.setKeyRepeatEnabled(false);
+	Window.setFramerateLimit(60);
+	
+	sf::Texture bTexture;
+	sf::Texture playerTexture;
+	sf::Sprite playerSprite;
+	sf::Sprite bImage;
+	sf::SoundBuffer jump;
+	sf::Music music;
+	if (!bTexture.loadFromFile("../resources/deanzabackground.jpg"))
+		std::cout << "Could not load background image" << std::endl;
+	if (!playerTexture.loadFromFile("../resources/player_spritesheet.png"))
+		std::cout << "Could not load player image" << std::endl;
+	if (!music.openFromFile("../resources/background.wav"))
+		std::cout << "Could not load background music" << std::endl;
+	if (!jump.loadFromFile("../resources/jump.wav"))
+		std::cout << "Could not load jump sound effect" << std::endl;
+	sf::Sound sound;
+	sound.setBuffer(jump);
 
-    bImage.setTexture(bTexture);
-    bImage.setScale(9.0f, (float)screenDimensions.y / bTexture.getSize().y);
+	bTexture.setSmooth(true);
+	bTexture.setRepeated(true);
+	bImage.setTexture(bTexture);
+	bImage.setScale(1.0f, 1.0f * screenDimensions.y / bTexture.getSize().y);
 
-    sf::RectangleShape rect(sf::Vector2f(20, 20));
-    rect.setFillColor(sf::Color::Red);
-    rect.setPosition(40, screenDimensions.y-20);
-    sf::Clock clock;
+	playerSprite.setTexture(playerTexture);
+	int playerWidth = 108;
+	int playerHeight = 140;
+	sf::IntRect playerRect(0, 0, playerWidth, playerHeight);
+	playerSprite.setTextureRect(playerRect);
 
-    float moveSpeed = 10000.0f;
-    float xVel = 10000.0f;
-    float yVel = 0.0f;
+	playerSprite.setPosition(80, screenDimensions.y - playerHeight);
+	sf::RectangleShape rect(sf::Vector2f(20, 20));
+	rect.setFillColor(sf::Color::Red);
+	rect.setPosition(80, screenDimensions.y - 20);
+	sf::Clock clock;
+	sf::Time time;
+	float frametime = 1.0f / 60.0f; //Updates 60 times per second
+	int animationRate = 5; //Animates once every 5 updates
+	int animationCtr = 0;
 
-    float xAcl = 0.0f;
-    float yAcl = 0.0f;
-    
-    sf::View view;
+	float xVel = 2.0f;
+	float yVel = 0.0f;
 
-    view.reset(sf::FloatRect(0, 0, screenDimensions.x, screenDimensions.y));
-    view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
+	float xAcl = 0.0f;
+	float yAcl = 0.0f;
 
-    sf::Vector2f position(screenDimensions.x / 2, screenDimensions.y / 2);
-    music.setLoop(true);
-    music.play();
-    while(Window.isOpen()){
-      clock.restart();
-      sf::Event Event;
-      while(Window.pollEvent(Event)){
-        switch(Event.type){
-          case sf::Event::Closed:  
-            Window.close();
-            break;
-            Window.close();
-            break;
-          case sf::Event::KeyPressed:
-            if(Event.key.code == sf::Keyboard::Escape){
+	sf::View view;
 
-            
-              Window.close();
-              
-            }
-            if(Event.key.code == sf::Keyboard::Space){
-                if (rect.getPosition().y == screenDimensions.y - 20)
-                {
-                    yAcl = 0.75f;
-                    sound.play();
-                }
-            }
+	view.reset(sf::FloatRect(0, 0, screenDimensions.x, screenDimensions.y));
+	view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
 
-            break;
-              
+	sf::Vector2f position(screenDimensions.x / 2, screenDimensions.y / 2);
+	music.setLoop(true);
+	music.play();
+	while (Window.isOpen())
+	{
+		time += clock.restart();
+		sf::Event Event;
+		if (time.asSeconds() >= frametime) //Sets a fixed time step so game can run at a constant update time
+		{
+			animationCtr++;
+			if(animationCtr == animationRate){
+				if(playerRect.left == playerWidth * 7)
+					playerRect.left = 0;
+				else
+					playerRect.left += playerWidth;
+				playerSprite.setTextureRect(playerRect);
+				animationCtr = 0;
 
-          
-        }
-      }
-      /*
-      if(yAcl > 2.0f){
-        yAcl = 2.0f;
-      }
-      */
+			}
+			while (Window.pollEvent(Event))
+			{
+				switch (Event.type)
+				{
+				case sf::Event::Closed:
+					Window.close();
+					break;
+					Window.close();
+					break;
+				case sf::Event::KeyPressed:
+					if (Event.key.code == sf::Keyboard::Escape)
+					{
 
-      if(rect.getPosition().y < screenDimensions.y - 20){
-        yAcl = -0.0005f;
-      }
+						Window.close();
+					}
+					if (Event.key.code == sf::Keyboard::Space)
+					{
+						if (playerSprite.getPosition().y == screenDimensions.y - playerHeight)
+						{
+							yAcl = 15.0f;
+							sound.play();
+						}
+					}
 
-      yVel += yAcl;
-      rect.setPosition(rect.getPosition().x, rect.getPosition().y - yVel);
-      
-      if(rect.getPosition().y > screenDimensions.y - 20){
-        rect.setPosition(rect.getPosition().x, screenDimensions.y - 20);
-      }
-      if(rect.getPosition().y == screenDimensions.y - 20){
-        yAcl = 0.0f;
-        yVel = 0.0f;
-      }
+					break;
+				}
+			}
 
-      position.x += .2;
-      rect.setPosition(position.x + 40, rect.getPosition().y);
-      /*
-      //rect.move(0, yVel )
-      yVel -= yAcl;
-      rect.setPosition(rect.getPosition().x, rect.getPosition().y + yVel);
-      /*
-      if(rect.getPosition().y >= 0 ){
-         yVel += yAcl;
-         rect.setPosition(rect.getPosition().x, rect.getPosition().y + yVel);
-      }
-      
-        
-      //rect.position.y = rect.getPosition().y + yVel;
-      
-     
-      if(rect.getPosition().y < screenDimensions.y - 20){
-        yAcl += .5f;
-      }else{
-        yAcl = 0.0f;
-        yVel = 0.0f;
-      }
-      std::cout << "yVel: " << yVel << " yPos: " << rect.getPosition().y << std::endl;
-      */
-      /*
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        rect.move(moveSpeed * clock.getElapsedTime().asSeconds(), 0);
-      else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        rect.move(-moveSpeed * clock.getElapsedTime().asSeconds(), 0);
-     
-      if(rect.getPosition().x + 10 > screenDimensions.x / 2)
-        position.x = rect.getPosition().x + 10;
-      else
-        position.x = screenDimensions.x / 2;
-      */
-      
-      /*
-      position.x = rect.getPosition().x + 10 - (screenDimensions.x / 2);
-      position.y = rect.getPosition().y + 10 - (screenDimensions.y / 2);
+			if (playerSprite.getPosition().y < screenDimensions.y - playerHeight)
+			{
+				yAcl = -0.4f;
+			}
 
-      if(position.x < 0)
-        position.x = 0;
-      if(position.y < 0)
-        position.y = 0;
-      */
-      //view.reset(sf::FloatRect(position.x, position.y, screenDimensions.x, screenDimensions.y));
-      //if(rect.getPosition().x + 10 > screenDimensions.x / 2)
+			yVel += yAcl;
 
-      view.setCenter(position);
-      Window.setView(view);
+			playerSprite.move(xVel, -yVel);
+			if (playerSprite.getPosition().y > screenDimensions.y - playerHeight)
+			{
+				playerSprite.setPosition(playerSprite.getPosition().x, screenDimensions.y - playerHeight);
+			}
+			if (playerSprite.getPosition().y == screenDimensions.y - playerHeight && (yAcl != 0.0f && yVel != 0.0f))
+			{
+				yAcl = 0.0f;
+				yVel = 0.0f;
+			}
 
-      Window.draw(bImage);
-      Window.draw(rect);
+			position.x += xVel;
 
-      Window.display();
-      Window.clear();
-    }
+			time -= sf::seconds(frametime);
+		}
 
-   
+		view.setCenter(position);
+		Window.setView(view);
 
-    return 0;
+		Window.draw(bImage);
+		Window.draw(playerSprite);
+
+		Window.display();
+		Window.clear();
+	}
+
+	return 0;
 }
