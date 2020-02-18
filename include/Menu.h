7f533048cde2ad7454d16f98ee61bf213c8c2e7a
dbiscoding <61105515+dbiscoding@users.noreply.github.com>
2020-02-17 20:51:18 -0800

    Add files via upload

--- include/Menu.h
+++ include/Menu.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "SFML/Graphics.hpp"
 
-const int MAX_NUMBER_OF_ITEMS = 4;
+const int MAX_NUMBER_OF_ITEMS = 3;
 class Menu
 {
 private:
 	int selectedItemIndex;
 	sf::Font font;
 	sf::Text menu[MAX_NUMBER_OF_ITEMS];
+	sf::Text option;
 
 public:
 	Menu(float width, float height);
