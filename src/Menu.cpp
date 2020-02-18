7eade4405b93a76321aee3728a30a93df2c815eb
dbiscoding <61105515+dbiscoding@users.noreply.github.com>
2020-02-17 19:26:38 -0800

    Add files via upload

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -4,31 +4,32 @@
 
 Menu::Menu(float width, float height)
 {
-	if (!font.loadFromFile("C:\\Users\\dbiso\\Downloads\\y.n.w.u.a.y.ttf"))
+	char file[] = "C:\\Users\\dbiso\\Downloads\\font.ttf";
+	if (!font.loadFromFile(file))
 	{
-		std::cout << "Error opening y.n.w.u.a.y.ttf\n";
+		std::cout << "Error opening \"" << file << "\"\n";
 		exit(EXIT_FAILURE);
 	}
 
 	menu[0].setFont(font);
 	menu[0].setColor(sf::Color::Red);
 	menu[0].setString("Play");
-	menu[0].setPosition(sf::Vector2f(width / 1.4, height / (MAX_NUMBER_OF_ITEMS + 1) * 1));
+	menu[0].setPosition(sf::Vector2f(width / 1.4, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
 
 	menu[1].setFont(font);
 	menu[1].setColor(sf::Color::Black);
 	menu[1].setString("Options");
-	menu[1].setPosition(sf::Vector2f(width / 1.4, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
+	menu[1].setPosition(sf::Vector2f(width / 1.4, height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
 
 	menu[2].setFont(font);
 	menu[2].setColor(sf::Color::Black);
 	menu[2].setString("Exit");
-	menu[2].setPosition(sf::Vector2f(width / 1.4, height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
+	menu[2].setPosition(sf::Vector2f(width / 1.4, height / (MAX_NUMBER_OF_ITEMS + 1) * 4));
 
 	menu[3].setFont(font);
 	menu[3].setColor(sf::Color::Black);
 	menu[3].setString("There are no options available right now");
-	menu[3].setPosition(sf::Vector2f(width / 1.4, height / (MAX_NUMBER_OF_ITEMS + 1) * 4));
+	menu[3].setPosition(sf::Vector2f(width / 1.4, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
 
 	selectedItemIndex = 0;
 }
