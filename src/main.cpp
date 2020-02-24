120b6acc53342fc0d6f3336df43256dd372d363a
Tuan <51724442+tuantruong5770@users.noreply.github.com>
2020-02-24 13:41:44 -0800

    added player class, disable obstacle use for now

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,20 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include "obstacle.h"
+#include "Menu.h"
+#include "player.h"
 #include <iostream>
 #include <string>
-#include "Menu.h"
+
 
 void mainMenu(sf::RenderWindow& Window, sf::Vector2i &screenDimensions);
 
 int main()
 {
-Again:	int noOfImgs = 7;
-	int xLocCharacter = 10;
-	int animationRate = 5; //Animates once every 5 updates
-	int levelIndex = 1; //For adjusting the rate in which obstacles are generated.
-	int animationCtr = 0;
-	int playerWidth = 108;
-	int playerHeight = 140;
+	int levelIndex = 11; //For adjusting the rate in which obstacles are generated.
 	float frametime = 1.0f / 60.0f; //Updates 60 times per second
 	float xVel = 11.0f;
-	float yVel = 0.0f;
-	float yAcl = 0.0f;
 	float takeOffSpeed = 22.0f;
-	float landingSpeed = -1.0f;
 	bool autoPlay = true;
 	bool autoJmped = true;
 	sf::Vector2i screenDimensions(800, 600);
@@ -41,7 +34,7 @@ Again:	int noOfImgs = 7;
 	sf::Event Event;
 	sf::RectangleShape** obstacleArr;
 	sf::Vector2f position(screenDimensions.x / 2, screenDimensions.y / 2);
-	sf::IntRect playerRect(0, 0, playerWidth, playerHeight);
+
 
 	Window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Late!");
 	Window.setKeyRepeatEnabled(false);
@@ -58,21 +51,24 @@ Again:	int noOfImgs = 7;
 	if (!squirrel.loadFromFile("../resources/squirrel.png"))
 		std::cout << "Could not load squirrel effect" << std::endl;
 
-	Obstacle obstacle(sf::Vector2i(screenDimensions.x, screenDimensions.y - 30), sf::Vector2f(67, 50), squirrel, 20);
+	//Obstacle obstacle(sf::Vector2i(screenDimensions.x, screenDimensions.y - 30), sf::Vector2f(67, 50), squirrel, 20);
+
+	Player player(screenDimensions, sf::Vector2i(108, 140), playerTexture, 7);
+	player.setXvelocity(xVel);
+	player.setJumpHeight(takeOffSpeed);
+
 
 	sound.setBuffer(jump);
 	bTexture.setSmooth(true);
 	bTexture.setRepeated(true);
 	bImage.setTexture(bTexture);
 	bImage.setScale(1.0f, 1.0f * screenDimensions.y / bTexture.getSize().y);
-	playerSprite.setTexture(playerTexture);
-	playerSprite.setTextureRect(playerRect);
-	playerSprite.setPosition(xLocCharacter, screenDimensions.y - playerHeight);
 	view.reset(sf::FloatRect(0, 0, screenDimensions.x, screenDimensions.y));
 	view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
 	music.setLoop(true);
 	music.play();
 	////////////////////////////
+	mainMenu:
 	mainMenu(Window, screenDimensions);
 	////////////////////////////
 	time = clock.restart();
@@ -82,17 +78,6 @@ Again:	int noOfImgs = 7;
 		time += clock.restart();
 		if (time.asSeconds() >= frametime) //Sets a fixed time step so game can run at a constant update time
 		{
-			animationCtr++;
-			if (animationCtr == animationRate)
-			{
-				if (playerRect.left == playerWidth * noOfImgs)
-					playerRect.left = 0;
-				else
-					playerRect.left += playerWidth;
-				playerSprite.setTextureRect(playerRect);
-				animationCtr = 0;
-			}
-
 			while (Window.pollEvent(Event))
 			{
 				switch (Event.type)
@@ -107,63 +92,29 @@ Again:	int noOfImgs = 7;
 					}
 					if (Event.key.code == sf::Keyboard::Space)
 					{
-						if (playerSprite.getPosition().y == screenDimensions.y - playerHeight)
+						if (player.jump())
 						{
-							yAcl = takeOffSpeed;
 							sound.play();
 						}
 					}
 					break;
 				}
 			}
-
-			if (playerSprite.getPosition().y < screenDimensions.y - playerHeight)
-			{
-				yAcl = landingSpeed;
-			}
-
-			else if (playerSprite.getPosition().y > screenDimensions.y - playerHeight)
-			{
-				playerSprite.setPosition(playerSprite.getPosition().x, screenDimensions.y - playerHeight);
-			}
-
-			yVel += yAcl;
-			playerSprite.move(xVel, -yVel);
 			position.x += xVel;
-
-			if (playerSprite.getPosition().y == screenDimensions.y - playerHeight)
-			{
-				yAcl = 0.0f;
-				yVel = 0.0f;
-			}
+			player.update();
 			time -= sf::seconds(frametime);
 		}
 		view.setCenter(position);
 		Window.setView(view);
 		Window.draw(bImage);
-		Window.draw(playerSprite);
-		if (autoJmped)
-		{
-			autoJmped = !obstacle.generateObstacle(position.x, time.asMicroseconds(), levelIndex);
-		}
-		for (int i = 0; i < obstacle.getCounter(); i++)
+		Window.draw(player.getSprite());
+		//obstacle.generateObstacle(position.x, time.asMicroseconds(), levelIndex);
+		/*for (int i = 0; i < obstacle.getCounter(); i++)
 		{
 			Window.draw(*(obstacle.getObstacle()[i]));
-			if (obstacle.collision(playerSprite, i, autoPlay))
-			{
-				if ((playerSprite.getPosition().y == screenDimensions.y - playerHeight) && autoPlay)
-				{
-					yAcl = takeOffSpeed;
-					sound.play();
-					autoJmped = true;
-				}
-				else 
-				{
-					std::cout << "GAME OVER" << std::endl;
-					goto Again; // replace this for game over or whatever
-				}
-			}
-		}
+			if (obstacle.collision(player.getSprite(), i))
+				Window.close();
+		}*/
 		Window.display();
 		Window.clear();
 	}
@@ -184,7 +135,8 @@ void mainMenu(sf::RenderWindow& Window, sf::Vector2i& screenDimensions)
 	while (Window.isOpen())
 	{
 		sf::Event event;
-	again:		Window.clear();
+	again:		
+		Window.clear();
 		Window.draw(menuBack);
 		menu.draw(Window);
 
